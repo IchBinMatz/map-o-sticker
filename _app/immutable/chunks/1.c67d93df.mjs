d1ebc56119e7b1a4d6a4ebccfd682e2729b69096
gpmatz <m.klint@gp-joule.de>
2023-02-24 11:33:03 +0100

    Updates

--- _app/immutable/chunks/1.c67d93df.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.69dd55a8.mjs";export{t as component};
---
+++ _app/immutable/chunks/2.52725d61.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.4ae3d38d.mjs";export{t as component};
