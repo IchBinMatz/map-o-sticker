2ca7640cc2867cfacb52caec154a2f58af8560fd
gpmatz <m.klint@gp-joule.de>
2023-03-11 15:34:26 +0100

    Updates

---
+++ _app/immutable/chunks/1.b5becdad.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.2b07f5f4.mjs";export{t as component};
---
+++ _app/immutable/chunks/2.c2af698c.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.24ffd64a.mjs";export{t as component};
